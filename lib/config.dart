8ad8fba47f17910c772b87fffe53189db625c0a4
Danillo Ilggner <danilloilggner@gmail.com>
2022-08-13 21:23:56 -0300

    fix: env

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,7 @@
 class Config {
-  static String get apiUrl =>
-      const String.fromEnvironment('API_URL', defaultValue: '');
+  static Uri get apiUri =>
+      Uri.parse(const String.fromEnvironment('API_URL', defaultValue: ''));
+
   static String get environment =>
       const String.fromEnvironment('ENVIRONMENT', defaultValue: 'development');
 }
