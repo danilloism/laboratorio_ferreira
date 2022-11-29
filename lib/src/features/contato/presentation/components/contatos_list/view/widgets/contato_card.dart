5cc42bbac7e601469a9409700775140da82f2c01
Danillo Ilggner <danilloilggner@gmail.com>
2022-11-29 12:02:15 -0300

    adicionar divider

--- lib/src/features/contato/presentation/components/contatos_list/view/widgets/contato_card.dart
+++ lib/src/features/contato/presentation/components/contatos_list/view/widgets/contato_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:laboratorio_ferreira_mobile/src/core/misc/extensions/build_context_extension.dart';
 
 import 'package:laboratorio_ferreira_mobile/src/features/contato/domain/models/contato.dart';
 
@@ -18,8 +19,14 @@ class ContatoCard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(children: [
               Row(
-                children: [Text(contato.nome)],
+                children: [
+                  Text(
+                    contato.nome,
+                    style: context.theme.textTheme.titleMedium,
+                  ),
+                ],
               ),
+              const Divider(),
             ]),
           ),
         ),
