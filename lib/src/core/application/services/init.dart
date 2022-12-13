e9e2dbb6b08c1ea819a6692d8e1390bf13b13626
Danillo Ilggner <danilloilggner@gmail.com>
2022-12-13 09:11:41 -0300

    pequenas alterações

--- lib/src/core/application/services/init.dart
+++ lib/src/core/application/services/init.dart
@@ -20,12 +20,9 @@ import 'package:loggy/loggy.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
-import 'dart:io' show Platform;
 
 class Init {
   const Init._();
-
-  static bool get _platformIsWindows => Platform.isWindows;
 
   static Future<ProviderContainer> get container async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -38,16 +35,10 @@ class Init {
       observers: [RiverpodLogger()],
     );
 
-    if (_platformIsWindows) {
-      await Future.wait([
-        Init._initDatabase(container),
-      ]);
-    } else {
-      await Future.wait([
-        Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
-        Init._initDatabase(container),
-      ]);
-    }
+    await Future.wait([
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+      Init._initDatabase(container),
+    ]);
 
     await _initSettingsRepo(container);
     _initPostAsyncCalls();
